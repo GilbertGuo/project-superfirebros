27e4726ca3ad6013f7e218a46d44b2079f100024
samuel.zhu <samuel.zhu@mail.utoronto.ca>
2019-03-27 18:19:32 -0400

    add home screen logo and logic

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ToastrService} from "ngx-toastr";
 import {GameService} from "../_services/game.service";
+import {UtilityService} from "../utility.service";
 
 @Component({
   selector: 'app-nav',
@@ -12,6 +13,6 @@ export class NavComponent implements OnInit {
   ngOnInit() {
   }
 
-  constructor(private toastr: ToastrService, private gameService: GameService) {
+  constructor(private toastr: ToastrService, private gameService: GameService, public utility: UtilityService) {
   }
 }
