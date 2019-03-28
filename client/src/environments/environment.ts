cb69103900190177df505c357c9b4ea614b7cd4d
samuel.zhu <samuel.zhu@mail.utoronto.ca>
2019-03-27 21:35:11 -0400

    added helmet for sercuiry

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  url:"https://localhost:9000",
+  url:"http://localhost:9000",
   production: false
 };
 
